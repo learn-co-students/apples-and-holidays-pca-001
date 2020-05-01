5d8bbe9bee18bfaec5ccccf33373dc890b6e09ac
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-01 16:12:22 -0400

    lab complete

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,40 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].each do |k,v|
+    holiday_hash[:winter][k].push(supply)
+  end
+  holiday_hash
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day].push(supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  arr = []
+  holiday_hash.each do |k0,v0| 
+    v0.each{ |k1, v1| arr.push(v1) }
+  end
+  arr.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +62,22 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |k0,v0|
+    puts "#{k0.to_s.capitalize}:"
+    v0.each do |k1,v1|
+      puts "  #{k1.to_s.split('_').collect{|c| c.capitalize }.join(' ')}: #{v1.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  ans = []
+  holiday_hash.each do |k0,v0|
+    v0.each do |k1,v1|
+      ans.push(k1) if v1.include? "BBQ"
+    end
+  end
+  ans
 end
-
-
-
-
-
-
-
